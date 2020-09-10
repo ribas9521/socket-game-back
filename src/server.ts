a35f9eeaac6c89389b52783e325861c90cb9c00d
ribas9521 <guilherme.rifer@gmail.com>
2020-09-10 14:58:05 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+// import app from './app'
+import socketio from 'socket.io'
+import http from 'http'
+import SocketControler from './controller/Socket'
+
+const server = http.createServer()
+
+const io = socketio(server)
+
+const socketController = new SocketControler(io)
+socketController.setEventListeners()
+
+server.listen(3333)
